977310555c35d90feb5d697bc1facca705a2d2bd
Ivan Lebedev <90276875+Qvvan@users.noreply.github.com>
2024-11-15 15:05:41 +0300

    fix

--- telegram_bot/logger/logging_config.py
+++ telegram_bot/logger/logging_config.py
@@ -1,6 +1,4 @@
 import logging
-import traceback
-from logging.handlers import RotatingFileHandler
 
 from aiogram import Bot
 from aiogram.client.default import DefaultBotProperties
@@ -10,28 +8,12 @@ from config_data import config
 
 
 class CustomLogger:
-    def __init__(self, name: str, log_file: str = "bot.log", max_file_size: int = 5 * 1024 * 1024, backup_count: int = 5):
+    def __init__(self, name: str):
         self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)  # Логгируем все, включая DEBUG
-
-        # Формат логов
-        log_format = logging.Formatter(
-            "%(levelname)s - %(asctime)s - [%(name)s] - [%(module)s.%(funcName)s] - line: %(lineno)d - %(message)s"
+        logging.basicConfig(
+            format="%(levelname)s - %(asctime)s - %(name)s - %(filename)s - line: %(lineno)d - %(message)s",
+            level=logging.INFO
         )
-
-        # Вывод логов в файл с ротацией
-        file_handler = RotatingFileHandler(log_file, maxBytes=max_file_size, backupCount=backup_count)
-        file_handler.setFormatter(log_format)
-        file_handler.setLevel(logging.DEBUG)
-
-        # Вывод логов в консоль
-        console_handler = logging.StreamHandler()
-        console_handler.setFormatter(log_format)
-        console_handler.setLevel(logging.INFO)
-
-        # Добавляем хендлеры к логгеру
-        self.logger.addHandler(file_handler)
-        self.logger.addHandler(console_handler)
 
     @staticmethod
     async def notify_group(message: str, error: Exception = None, keyboard=None):
@@ -39,11 +21,8 @@ class CustomLogger:
         group_id = config.ERROR_GROUP_ID if error else config.INFO_GROUP_ID
         notification_type = "🚨 Ошибка:\n" if error else "ℹ️ Информация:\n"
 
-        # Формируем сообщение об ошибке
-        error_message = ""
-        if error:
-            error_message = f"\n\nОшибка: {str(error)}\n" \
-                            f"Трассировка:\n<pre>{traceback.format_exc()}</pre>"
+        # Добавляем текст ошибки, если она есть
+        error_message = f"\n\nОшибка: {str(error)}" if error else ""
 
         async with Bot(
                 token=config.BOT_TOKEN,
@@ -55,31 +34,24 @@ class CustomLogger:
                 reply_markup=keyboard
             )
 
-    async def log_debug(self, message: str):
-        """Логгирование отладочной информации."""
-        self.logger.debug(message, stacklevel=3)
-
     async def log_info(self, message: str):
         """Логгирование информации и отправка уведомления."""
-        self.logger.info(message, stacklevel=3)
+        self.logger.info(message)
         await self.notify_group(message)
-
-    async def log_warning(self, message: str):
-        """Логгирование предупреждений."""
-        self.logger.warning(message, stacklevel=3)
 
     async def log_error(self, message: str, error: Exception, keyboard=None):
         """Логгирование ошибки и отправка уведомления."""
         full_message = f"{message}. Error: {str(error)}"
-        self.logger.error(full_message, exc_info=True, stacklevel=3)
+        self.logger.error(full_message)
         await self.notify_group(message, error, keyboard)
 
-    async def log_critical(self, message: str, error: Exception, keyboard=None):
-        """Логгирование критической ошибки и отправка уведомления."""
-        full_message = f"{message}. Critical Error: {str(error)}"
-        self.logger.critical(full_message, exc_info=True, stacklevel=3)
-        await self.notify_group(message, error, keyboard)
+    async def info(self, message):
+        self.logger.info(message)
+
+    async def error(self, message: str, error):
+        full_message = f"{message}. Error: {str(error)}"
+        self.logger.error(full_message)
 
 
-# Экземпляр логгера
+# Экземпляр логгера для использования в других модулях
 logger = CustomLogger(__name__)
