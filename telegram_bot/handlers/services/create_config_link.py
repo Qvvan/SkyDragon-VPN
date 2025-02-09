d1eeda5a08e6dba81409cd46e2d24208eb61eccf
Qvvan <ivan_ivan_lebedev@mail.ru>
2025-02-09 17:59:10 +0300

    fix

--- telegram_bot/handlers/services/create_config_link.py
+++ telegram_bot/handlers/services/create_config_link.py
@@ -3,6 +3,6 @@ from utils.encode_link import encrypt_part
 
 async def create_config_link(user_id: int, sub_id: int):
     encoded_user_id = encrypt_part(str(user_id) + "|" + str(sub_id))
-    url = "https://skydragonvpn/sub/"
+    url = "https://skydragonvpn.ru/sub/"
 
     return url + encoded_user_id
